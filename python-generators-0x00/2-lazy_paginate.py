aa260fef5614faa422bf0865d551535002102ff6
Brenda Bonareri <brendabjb24@gmail.com>
2025-05-19 00:53:40 +0300

    Corrected 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lazy loading Paginated Data: fetch paginated batches of users lazily.
+Lazy loading Paginated Data: fetch paginated batches of users lazily using generators.
 """
 import seed
 
@@ -13,7 +13,7 @@ def paginate_users(page_size, offset):
     conn = seed.connect_to_prodev()
     cursor = conn.cursor(dictionary=True)
     cursor.execute(
-        "SELECT user_id, name, email, age FROM user_data LIMIT %s OFFSET %s",
+        "SELECT * FROM user_data LIMIT %s OFFSET %s",
         (page_size, offset)
     )
     rows = cursor.fetchall()
@@ -25,13 +25,17 @@ def paginate_users(page_size, offset):
 def lazypaginate(page_size):
     """
     Generator that yields pages (lists) of users lazily, fetching only when needed at an offset starting at 0.
+
+    Prototype:
+        def lazypaginate(page_size)
     """
     offset = 0
-    while True:  # single loop for lazy pagination
+    while True:  # only loop
         page = paginate_users(page_size, offset)
         if not page:
             break
         yield page
         offset += page_size
+
 
 
