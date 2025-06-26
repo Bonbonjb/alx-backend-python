773bc6a13fadb2f7e630bcb264e6d35ede8ae158
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-06-27 01:23:16 +0300

    Update auth.py

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class CustomJWTAuthentication(JWTAuthentication):
+    """
+    Placeholder for future customization of JWT auth if needed.
+    """
+    pass
