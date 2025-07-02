2f540da7cd626cbbebca681caa91a2cfda5e0898
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-02 11:05:35 +0300

    Delete messaging_app directory

--- messaging_app/chats/auth.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """
-    Placeholder for future customization of JWT auth if needed.
-    """
-    pass
