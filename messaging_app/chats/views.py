b5a6ab23171b26cb47be10a915f98dc36b2cf55f
Brenda Bonareri <brendabjb24@gmail.com>
2025-07-02 12:36:05 +0300

    Add ConversationViewSet and MessageViewSet endpoints

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get("conversation_pk") or self.request.query_params.get("conversation_id")
+        conversation = get_object_or_404(Conversation, pk=conversation_id)
+
+        # Check permission
+        self.check_object_permissions(self.request, conversation)
+        return Message.objects.filter(conversation=conversation).order_by("sent_at")
+
+    def perform_create(self, serializer):
+        conversation_id = self.request.data.get("conversation_id")
+        conversation = get_object_or_404(Conversation, pk=conversation_id)
+
+        # Check permission
+        self.check_object_permissions(self.request, conversation)
+
+        serializer.save(sender=self.request.user, conversation=conversation)
