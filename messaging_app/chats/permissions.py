c5703105f28585f16ce58fc128ec74d319005f8d
Brenda Bonareri <brendabjb24@gmail.com>
2025-07-02 14:53:58 +0300

    Authentication and Permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,3 +3,8 @@ from rest_framework import permissions
 class IsParticipantOfConversation(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()
+
+class IsOwnerOrParticipant(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or request.user in obj.participants.all()
+       
