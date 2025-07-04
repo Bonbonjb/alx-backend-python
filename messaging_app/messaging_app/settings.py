12dd5c58ec02d980013d01ae976c847d23030b22
Brenda Bonareri <bonarerijb24@gmail.com>
2025-07-04 16:34:41 +0300

    Added docker compose

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DB'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': '3306',
     }
 }
 
