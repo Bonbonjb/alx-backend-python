6f0312e33d02f8bca5f09bc6ca9048e0a549e024
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-30 14:04:33 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -87,7 +87,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
-    def test_has_license(self, repo: Dict, license_key: str, expected_value: bool) -> None:
+    def test_has_license(self, repo, license_key, expected_value):
         """
         Test that has_license correctly checks for the presence of a license.
         """
