2ed36a9ab94874f48dff5dafd50135e7c0cd8927
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-08-17 14:42:36 +0300

    Update apps.py

--- Django-signals_orm_0x04/messaging/apps.py
+++ Django-signals_orm_0x04/messaging/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class MessagingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messaging'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "messaging"
+
+    def ready(self):
+        from . import signals  # noqa: F401 - ensure signal handlers are registered
+
